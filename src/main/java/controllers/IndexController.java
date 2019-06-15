06a7f8200e3005bebdbe94d0d4eaf8321e8242a6
Samiksha Gautam <samikshyagautam5@gmail.com>
2019-06-15 18:06:16 -0500

    initial change

--- src/main/java/controllers/IndexController.java
+++ src/main/java/controllers/IndexController.java
@@ -1,4 +1,17 @@
 package controllers;
 
-public class IndexController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("home.jsp").forward(req,resp);
+
+    }
 }
